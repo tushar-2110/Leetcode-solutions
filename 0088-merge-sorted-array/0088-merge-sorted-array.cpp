27507e11d8c50ef11cc84499d1722742f215cc65
Tushar Sharma <82231061+tushar-2110@users.noreply.github.com>
2023-01-16 13:19:04 +0530

    Time: 0 ms (100.00%), Space: 9.1 MB (74.43%) - LeetHub

---
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -0,0 +1,19 @@
+class Solution
+{
+    public:
+        void merge(vector<int> &nums1, int m, vector<int> &nums2, int n) {
+            
+            if(m<0)
+                return;
+            
+           
+            
+            for(int i=m;i<n+m;i++)
+                nums1[i]=nums2[i-m];
+            
+            sort(nums1.begin(),nums1.end());
+            
+            
+            
+        }
+};

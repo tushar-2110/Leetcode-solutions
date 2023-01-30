1a44362e22883a9c4f93d76d81a98d8b25259d1a
Tushar Sharma <82231061+tushar-2110@users.noreply.github.com>
2023-01-30 20:01:06 +0530

    Time: 32 ms (91.73%), Space: 24.9 MB (19.55%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
@@ -2,27 +2,38 @@ class Solution {
 public:
     vector<int> nextGreaterElements(vector<int>& nums) {
         
+         int n = nums.size();
+        nums.resize(2*n);
         
-          vector<int> ans;
-        bool flag;
-        for(int i=0;i<nums.size();i++){
-            flag=false;
-            for(int j=i+1;j<2*nums.size()-1;j++){
-                if(nums[i] < nums[ j% nums.size()]){
-                    ans.push_back(nums[j%nums.size()]);
-                    flag=true;
-                    break;
-                    
+        for(int i=n; i<2*n; i++) //concatenate the same array
+        {
+            nums[i] = nums[i-n];
+        }
+        
+        vector<int> res(n, -1); //to be returned, initialize it with -1
+        stack<int> st;
+        
+        for(int i=0; i<2*n; i++)
+        {
+            int ele = nums[i];
+            
+            while(!st.empty() && ele > nums[st.top()])
+            {
+				//ele acts as NGE to the value at st.top()
+				
+                if(st.top() >= n) //index should not exceed n
+                {
+                    st.top() = st.top() - n;
                 }
-            }
-            if(!flag){
-                ans.push_back(-1);
+                
+                res[st.top()] = ele;
+                st.pop();
             }
             
+            st.push(i);
         }
-        return ans;
         
-        
+        return res;
         
         
     }
